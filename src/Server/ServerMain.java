8d3d114f7675adb5a83f7e8a7e5e0ea0db1af41c
Yazan <yazan.sr.123.h@outlook.com>
2025-11-23 22:24:25 +0200

    fixing files so we connect right

--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -1,8 +1,9 @@
-import java.net.ServerSocket;
-import java.net.Socket;
+package Server;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerMain {
 
