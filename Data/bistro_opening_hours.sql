60c2df66995b3757e8c7f44fba422cf8ff5c4253
Naeem Harb / software engineering <nanahaha2244@gmail.com>
2025-12-29 17:55:25 +0200

    Add files via upload
    sql tables the database

---
+++ Data/bistro_opening_hours.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bistro
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `opening_hours`
+--
+
+DROP TABLE IF EXISTS `opening_hours`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `opening_hours` (
+  `hours_id` int NOT NULL AUTO_INCREMENT,
+  `day_of_week` enum('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday') NOT NULL,
+  `open_time` time NOT NULL,
+  `close_time` time NOT NULL,
+  `is_special` enum('YES','NO') DEFAULT 'NO',
+  `special_date` date DEFAULT NULL,
+  PRIMARY KEY (`hours_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `opening_hours`
+--
+
+LOCK TABLES `opening_hours` WRITE;
+/*!40000 ALTER TABLE `opening_hours` DISABLE KEYS */;
+INSERT INTO `opening_hours` VALUES (1,'Sunday','09:00:00','22:00:00','NO',NULL),(2,'Monday','09:00:00','22:00:00','NO',NULL),(3,'Tuesday','09:00:00','22:00:00','NO',NULL),(4,'Wednesday','09:00:00','22:00:00','NO',NULL),(5,'Thursday','09:00:00','22:00:00','NO',NULL),(6,'Friday','09:00:00','22:00:00','NO',NULL),(7,'Saturday','09:00:00','22:00:00','NO',NULL),(8,'Sunday','09:00:00','22:00:00','NO',NULL),(9,'Monday','09:00:00','22:00:00','NO',NULL),(10,'Tuesday','09:00:00','22:00:00','NO',NULL);
+/*!40000 ALTER TABLE `opening_hours` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-28 13:45:46
