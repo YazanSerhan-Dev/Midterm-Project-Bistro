9c1104d88f105a17877397f858c9d86c817cb366
nadeenhaj2 <nadenhaj40@gmail.com>
2026-01-17 02:15:50 +0200

    Javadock - common.dto package in Bistro_Client

--- Bistro_Client/src/common/dto/LoginRequestDTO.java
+++ Bistro_Client/src/common/dto/LoginRequestDTO.java
@@ -2,22 +2,36 @@ package common.dto;
 
 import java.io.Serializable;
 
+/**
+ * DTO representing a login request sent from client to server.
+ *
+ * <p>
+ * Contains the credentials required for authentication.
+ * Used for both subscriber and staff login requests.
+ * </p>
+ */
 public class LoginRequestDTO implements Serializable {
-
+    /** Username provided by the user */
     private String username;
+    /** Plain-text password provided by the user */
     private String password;
-
+    /** Required no-args constructor for serialization */
     public LoginRequestDTO() {}
-
+    /**
+     * Constructs a login request with credentials.
+     *
+     * @param username user's username
+     * @param password user's password
+     */
     public LoginRequestDTO(String username, String password) {
         this.username = username;
         this.password = password;
     }
-
+    /** @return the username */
     public String getUsername() {
         return username;
     }
-
+    /** @return the password */
     public String getPassword() {
         return password;
     }
